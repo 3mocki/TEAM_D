31afda97d2dd8a403f52bc7385a354469cd2c3e8
3mocki <starleesm@gmail.com>
2019-02-20 16:54:20 -0800

    20190219

--- bthandler.py
+++ bthandler.py
@@ -66,54 +66,37 @@ class BTClientHandler(asyncore.dispatcher_with_send):
         asyncore.dispatcher_with_send.__init__(self, socket)
         self.server = server
         self.data = ""
-        global gSocket
-        gSocket = self
 
     def handle_read(self):
         try:
-            data = self.recv(1024)
+            data = self.recv(1024).decode('ascii')
             if not data:
                 return
 
-            # print (data)
-            # curTime = data.split(',')[1]
-            # curTime = datetime.datetime.strptime(curTime.strip('\n'), "%Y-%m-%d %H:%M:%S")
-            # global gCurTime
-            # gCurTime = curTime
-            global gBoardID
-            gBoardID = int(data.split(',')[2])
             lf_char_index = data.find('\n')
 
             if lf_char_index == -1:
                 # No new line character in data, so we append all.
                 self.data += data
             else:
+                # We see a new line character in data, so append rest and handle.
                 self.data += data[:lf_char_index]
-                #a = []
-                if self.data.split(',')[0] == "sensor":
-                    with lock:
-                        global bConnectApp
-                        bConnectApp = True
-                        # global bSensHisOnce
-                        # bSensHisOnce = True
+                print("received [{}]".format(self.data))
+
+                self.send((self.data + '\n').encode('ascii'))
+
 
                 # Clear the buffer
                 self.data = ""
-
         except Exception as e:
             BTError.print_error(handler=self, error=BTError.ERR_READ, error_message=repr(e))
             self.data = ""
-            lf_char_index = -1
-            with lock:
-                global testValue
-                testValue = False
             self.handle_close()
 
     def handle_close(self):
         # flush the buffer
         while self.writable():
             self.handle_write()
-        global bConnectApp
-        bConnectApp = False
+
         self.server.active_client_handlers.remove(self)
         self.close()
